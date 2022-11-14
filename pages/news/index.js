8b1a7d8cfe5e70daba7fea6bbc234adf0020d503
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-11-14 21:17:46 +0200

    add linking beetween pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,19 @@
+import {Fragment} from "react";
+import Link from 'next/link'
+
 function NewsPage() {
     return (
-        <h1>The News Page</h1>
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/next-js-is-great">
+                        NextJS is a Great!
+                    </Link>
+                </li>
+                <li>Python is a Awesome!</li>
+            </ul>
+        </Fragment>
     )
 }
 
