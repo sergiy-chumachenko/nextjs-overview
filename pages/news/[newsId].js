28b2c621b3552d88975125a353669e58c716a6cc
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-11-14 21:08:50 +0200

    extract dynamic page parameters

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import {useRouter} from "next/router";
+
 function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+    // send a request to backend API using newsId
     return (
         <h1>The DetailPage Page</h1>
     )
